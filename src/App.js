ec2c636bd0297cbdda71b46a15efe4c1466a6b85
AKeyNo <arielaquino.cs@gmail.com>
2021-01-07 21:41:27 -0800

    created skeleton app bar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect,
-} from "react-router-dom";
-
-import { Navbar } from './app/Navbar'
+import { Switch, Route, Link, BrowserRouter, Redirect } from "react-router-dom";
+import { AppBar, Tabs, Tab } from "@material-ui/core";
+import HomeIcon from '@material-ui/icons/Home';
+import CommentIcon from "@material-ui/icons/Comment";
+import AccountCircleIcon from "@material-ui/icons/AccountCircle";
 
 function App() {
+  const routes = ["/", "/tab2", "/tab3"];
   return (
-    <Router>
-      <Navbar />
-      <div className="App">
+    <div className="App">
+      <BrowserRouter>
+        <Route
+          path="/"
+          render={(history) => (
+            <AppBar position="static">
+              <Tabs
+                value={
+                  history.location.pathname !== "/"
+                    ? history.location.pathname
+                    : false
+                }
+                centered
+              >
+                {console.log(history.location.pathname)}
+                <Tab
+                  value={routes[0]}
+                  label="Home"
+                  icon={<HomeIcon />}
+                  component={Link}
+                  to={routes[0]}
+                />
+                <Tab
+                  value={routes[1]}
+                  label="Saying"
+                  icon={<CommentIcon />}
+                  component={Link}
+                  to={routes[1]}
+                />
+                <Tab
+                  value={routes[2]}
+                  label="Profile"
+                  icon={<AccountCircleIcon />}
+                  component={Link}
+                  to={routes[2]}
+                />
+              </Tabs>
+            </AppBar>
+          )}
+        />
         <Switch>
-          <Route exactpath="/" render={() => <ul>test message</ul>}></Route>
+          <Route
+            exact
+            path="/"
+            render={() => <React.Fragment>Home Tab</React.Fragment>}
+          />
+          <Route
+            exact
+            path="/tab2"
+            render={() => <React.Fragment>Saying Tab</React.Fragment>}
+          />
+          <Route
+            exact
+            path="/tab3"
+            render={() => <React.Fragment>Profile Tab</React.Fragment>}
+          />
           <Redirect to="/" />
         </Switch>
-      </div>
-    </Router>
+      </BrowserRouter>
+    </div>
   );
 }
 
